f99b5eab9a8aebd3f783d54b65f7261114b6cc1f
laua5 <laua5@middleton.school.nz>
2024-05-08 13:59:04 +1200

    Create 03_add_card_v1.py

---
+++ 03_add_card_v1.py
@@ -0,0 +1,43 @@
+"""Adding component v1 - allows user to add a card"""
+
+
+# Existing cards
+cards = {
+     "Stoneling": {"Strength": 7, "Speed": 1,
+                   "Stealth": 25, "Cunning": 15},
+     "Vexscream": {"Strength": 1, "Speed": 6,
+                   "Stealth": 21, "Cunning": 19},
+     "Dawnmirage": {"Strength": 5, "Speed": 15,
+                    "Stealth": 18, "Cunning": 22},
+     "Blazegolem": {"Strength": 15, "Speed": 20,
+                    "Stealth": 23, "Cunning": 6},
+     "Websnake": {"Strength": 7, "Speed": 15,
+                  "Stealth": 10, "Cunning": 5},
+     "Moldvine": {"Strength": 21, "Speed": 18,
+                  "Stealth": 14, "Cunning": 5},
+     "Vortexwing": {"Strength": 19, "Speed": 13,
+                    "Stealth": 19, "Cunning": 2},
+     "Rotthing": {"Strength": 16, "Speed": 7,
+                  "Stealth": 4, "Cunning": 12},
+     "Froststep": {"Strength": 14, "Speed": 14,
+                   "Stealth": 17, "Cunning": 4},
+     "Wispghoul": {"Strength": 17, "Speed": 19,
+                   "Stealth": 3, "Cunning": 2}
+}
+
+while True:
+    the_name = input("Please enter name of new monster card(x to exit): ")
+    if the_name == "x":
+        break
+    else:
+        # Adds new monster card name to dictionary
+        cards[the_name] = {}
+        # Prints out full list of cards including the name of the card
+        # just entered by user
+        for card_name, card_info in cards.items():
+            print(f"\nCard name:{card_name}")
+            for key in card_info:
+                print(f"{key}:{card_info[key]}")
+
+
+
