408b9f0a7ee4c1600eaea3c5044960754e82423d
laua5 <laua5@middleton.school.nz>
2024-05-15 13:55:08 +1200

    Update 03_store_card_v2.py

--- 03_store_card_v2.py
+++ 03_store_card_v2.py
@@ -1,14 +1,32 @@
 """Store card v2 - using lists"""
 
-cards = [["Stonelling", 7, 1, 25, 15],["Vexscream", 1, 6, 21, 19],
-         ["Dawnmirage", 5, 15, 18, 22], ["Blazegolem", 15, 20, 23, 6],
-         ["Websnake", 7,15, 10, 5], ["Moldvine",21, 18, 14, 5],
-         ["Vortexwing", 19, 13, 19, 2], ["Rotthing", 16, 7, 4, 12],
-         ["Froststep", 14, 14, 17, 4], ["Wispyghoul", 17, 19, 3, 2]]
+# Existing cards
+cards = {"Stonelling": [["Strength", 7], ["Speed", 1], ["Stealth", 25],
+                        ["Cunning", 15]],
+         "Vexscream": [["Strength", 1], ["Speed", 6], ["Stealth", 21],
+                       ["Cunning", 19]],
+         "Dawnmirage": [["Strength", 5,], ["Speed", 15], ["Stealth", 18],
+                        ["Cunning",22]],
+         "Blazegolem": [["Strength", 15], ["Speed", 20], ["Stealth", 23],
+                        ["Cunning", 6]],
+         "Websnake": [["Strength", 7], ["Speed", 15], ["Stealth", 10],
+                      ["Cunning", 5]],
+         "Moldvine": [["Strength", 21], ["Speed", 18], ["Stealth", 14],
+                      ["Cunning", 5]],
+         "Vortexwing": [["Strength", 19], ["Speed", 13], ["Stealth", 19],
+                        ["Cunning", 2]],
+         "Rotthing": [["Strength", 16], ["Speed", 7], ["Stealth",  4],
+                      ["Cunning", 12]],
+         "Froststep": [["Strength", 14], ["Speed", 14], ["Stealth", 17],
+                       ["Cunning", 4]],
+         "Wispyghoul": [["Strength", 17], ["Speed", 19], ["Stealth",  3],
+                        ["Cunning", 2]]
+}
+
 
 for card in cards:
-    print(f"\nCard name:{card[0]}")
-    print(f"Strength:{card[1]}")
-    print(f"Speed:{card[2]}")
-    print(f"Stealth:{card[3]}")
-    print(f"Cunning:{card[4]}")
+        print(f"\nCard name:{card[0]}")
+        print(f"Strength:{card[1]}")
+        print(f"Speed:{card[2]}")
+        print(f"Stealth:{card[3]}")
+        print(f"Cunning:{card[4]}")
